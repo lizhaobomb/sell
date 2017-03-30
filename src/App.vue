44c0925452a65877cb8fddb8f67390f0e27dacbe
lizhaobomb <lizhaobomb@163.com>
2017-03-30 18:14:00 +0800

    添加1px边框
    添加1px边框

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -16,9 +16,26 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import header from './components/header/header.vue'
+  const ERR_OK = 0
+
   export default {
+    data() {
+      return {
+        seller: {}
+      }
+    },
+
+    created() {
+      this.$http.get('./api/seller').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.seller = response.data
+        }
+      })
+    },
+
     components: {
       'v-header': header
     }
@@ -26,12 +43,14 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/mixin.styl"
   #app
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
         text-align: center
